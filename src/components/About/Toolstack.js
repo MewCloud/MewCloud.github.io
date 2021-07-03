4d8e5e5b13edc003d9465f0f5d9f5cf0f965b31b
ductnn <trannhuduc531998@gmail.com>
2021-07-03 13:43:17 +0700

    Fix some toolstack

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,28 +3,30 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiLinux,
   SiVisualstudiocode,
-  SiJupyter,
+  SiJenkins,
   SiPostman,
-  SiHeroku,
 } from "react-icons/si";
+import {
+  DiTerminal
+} from "react-icons/di"
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiJupyter />
+        <SiJenkins />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiTerminal/>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHeroku />
       </Col>
     </Row>
   );
