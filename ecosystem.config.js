a017d6c49e4c2f5fc320e372d10b1bc80621a776
ductnn <trannhuduc531998@gmail.com>
2021-10-23 23:31:39 +0700

    Add NODE_ENV

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,14 @@ module.exports = {
     apps: [
         {
             name: 'mewcloud',
-            script: 'node index.js',
+            script: './server/index.js',
             ignore_watch: ['node_modules'],
             instances: 1,
             autorestart: true,
             max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'dev',
+            },
         },
     ],
 };
