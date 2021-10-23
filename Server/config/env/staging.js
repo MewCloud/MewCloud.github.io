3e140fb72275595ced1503d41441399dba983bc0
ductnn <trannhuduc531998@gmail.com>
2021-10-23 23:32:12 +0700

    Build server

---
+++ Server/config/env/staging.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'staging',
+  domain: 'https://cmsstagingreddiz.reddi.asia', // todo config
+  api: 'https://apistagingreddiz.reddi.asia',
+  port: 7878,
+  host: '0.0.0.0',
+};
